eaf5fc5ea8a31efd191f18222d7e59843915e4b2
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-07 05:28:35 +0800

    Version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,127 @@
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#define  WIFI_SSID ""
-#define  WIFI_PASS ""
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 const int RELAY1_PIN = D1;
 const int RELAY2_PIN = D2;
 ESP8266WebServer server(80);
+MDNSResponder MDNS;
 
-void handleRoot() {
-  String output = "<html><head><title>Hash'J HouseLights</title>";
-  output += "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">";
-  output += "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>";
-  output += "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>";
+bool BULBstatus1 = HIGH;
+bool BULBstatus2 = HIGH;
+
+void handleRoot()
+{
+  String output = "<html><head><title>SmartLight Control</title>";
+  output += "<style>html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}";
+  output += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}";
+  output += ".button {margin-top: 30px; display: block;background-color: #1abc9c;border: none;color: white;padding: 2em 3em;text-decoration: none;font-size: 5em;margin: 0px auto 35px;cursor: pointer;border-radius: 10px;}";
+  output += ".button-on {background-color: #3498db;}";
+  output += ".button-on:active {background-color: #3498db;}";
+  output += ".button-off {background-color: #34495e;}";
+  output += ".button-off:active {background-color: #2c3e50;}";
+  output += "p {font-size: 14px;color: #888;margin-bottom: 10px;}";
+  output += "</style>";
   output += "</head><body>";
-  output += "<div class=\"container\">";
-  output += "<h1>Hash'J HouseLights</h1>";
-  output += "<div class=\"row\">";
-  output += "<div class=\"col-md-6\">";
-  output += "<h2>Relay 1</h2>";
-  output += "<button id=\"relay1-on\" class=\"btn btn-success\">Turn On</button>";
-  output += "<button id=\"relay1-off\" class=\"btn btn-danger\">Turn Off</button>";
-  output += "</div>";
-  output += "<div class=\"col-md-6\">";
-  output += "<h2>Relay 2</h2>";
-  output += "<button id=\"relay2-on\" class=\"btn btn-success\">Turn On</button>";
-  output += "<button id=\"relay2-off\" class=\"btn btn-danger\">Turn Off</button>";
-  output += "</div>";
-  output += "</div>";
-  output += "</div>";
-  output += "<script>";
-  output += "$('#relay1-on').click(function() {";
-  output += "$.get('/relay1/on');";
-  output += "});";
-  output += "$('#relay1-off').click(function() {";
-  output += "$.get('/relay1/off');";
-  output += "});";
-  output += "$('#relay2-on').click(function() {";
-  output += "$.get('/relay2/on');";
-  output += "});";
-  output += "$('#relay2-off').click(function() {";
-  output += "$.get('/relay2/off');";
-  output += "});";
-  output += "</script>";
+  output += "<h1 style='font-size: 4em;'>SmartLight Control</h1>";
+  output += "<h1 style='font-size: 3em;'>Light Bulb 1</h1>";
+  if (BULBstatus1 == LOW)
+  {
+    output += "<a href='relay1/off' style='text-decoration:none'><button class='button button-off'>Turn Off</button></a>";
+  }
+  else if (BULBstatus1 == HIGH)
+  {
+    output += "<a href='relay1/on' style='text-decoration:none'><button class='button button-on'>Turn On</button></a>";
+  }
+  output += "<h1 style='font-size: 3em;'>Light Bulb 2</h1>";
+  if (BULBstatus2 == LOW)
+  {
+    output += "<a href='relay2/off' style='text-decoration:none'><button class='button button-off'>Turn Off</button></a>";
+  }
+  else if (BULBstatus2 == HIGH)
+  {
+    output += "<a href='relay2/on' style='text-decoration:none'><button class='button button-on'>Turn On</button></a>";
+  }
   output += "</body></html>";
   server.send(200, "text/html", output);
 }
 
-void handleRelay1On() {
+void handleRelay1On()
+{
   digitalWrite(RELAY1_PIN, LOW);
-  server.send(200, "text/plain", "Relay 1 turned on");
+  delay(100);
+  BULBstatus1 = LOW;
+  digitalWrite(LED_BUILTIN, LOW);
+  server.sendHeader("Location", String("/"), true);
+  server.send(302, "text/plain", "");
 }
 
-void handleRelay1Off() {
+void handleRelay1Off()
+{
   digitalWrite(RELAY1_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 1 turned off");
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  BULBstatus1 = HIGH;
+  server.sendHeader("Location", String("/"), true);
+  server.send(302, "text/plain", "");
 }
 
-void handleRelay2On() {
+void handleRelay2On()
+{
   digitalWrite(RELAY2_PIN, LOW);
-  server.send(200, "text/plain", "Relay 2 turned on");
+  delay(100);
+  BULBstatus2 = LOW;
+  server.sendHeader("Location", String("/"), true);
+  server.send(302, "text/plain", "");
 }
 
-void handleRelay2Off() {
+void handleRelay2Off()
+{
   digitalWrite(RELAY2_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 2 turned off");
+  delay(100);
+  BULBstatus2 = HIGH;
+  server.sendHeader("Location", String("/"), true);
+  server.send(302, "text/plain", "");
 }
 
-void setup() {
+void status()
+{
+  server.send(200, "text/plain", "Bulb 1 status: " + String(BULBstatus1) + "\nBulb 2 status: " + String(BULBstatus2));
+}
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY1_PIN, OUTPUT);
-  digitalWrite(RELAY1_PIN, HIGH);
   pinMode(RELAY2_PIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(RELAY1_PIN, HIGH);
   digitalWrite(RELAY2_PIN, HIGH);
-  WiFi.setHostname("houselights");
+  WiFi.softAP("SmartLight", "12345678");
+  WiFi.setHostname("SmartLight");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected to WiFi");
-  MDNS.begin("houselights");
-  Serial.println("MDNS responder started");
-  server.on("/", handleRoot);
-  server.on("/relay1/on", handleRelay1On);
-  server.on("/relay1/off", handleRelay1Off);
-  server.on("/relay2/on", handleRelay2On);
-  server.on("/relay2/off", handleRelay2Off);
-  server.begin();
-  Serial.println("HTTP server started");
+  if (MDNS.begin("SmartLight", WiFi.localIP()))
+  {
+    server.on("/", handleRoot);
+    server.on("/relay1/on", HTTP_GET, handleRelay1On);
+    server.on("/relay1/off", HTTP_GET, handleRelay1Off);
+    server.on("/relay2/on", HTTP_GET, handleRelay2On);
+    server.on("/relay2/off", HTTP_GET, handleRelay2Off);
+    server.on("/status", HTTP_GET, status);
+    server.begin();
+    MDNS.addService("http", "tcp", 80);
+  }
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
 }
